cc240617c374fef31fdca84657dbb16258eac9b6
22P31A4404 <119731243+22P31A4404@users.noreply.github.com>
2022-12-29 13:23:09 +0530

    #include<stdio.h> int main() {     char ch,n;     scanf("%ch",&n);     if(n=='A'||n=='a'||n=='E'||n=='e'||n=='I'||n=='i'||n=='O'||n=='o'||n=='I'||n=='i')     {         printf("Vowel");     }     else     {         printf("Consonant");     }
    }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch,n;
+    scanf("%ch",&n);
+    if(n=='A'||n=='a'||n=='E'||n=='e'||n=='I'||n=='i'||n=='O'||n=='o'||n=='I'||n=='i')
+    {
+        printf("Vowel");
+    }
+    else
+    {
+        printf("Consonant");
+    }
+    
+    
+}
