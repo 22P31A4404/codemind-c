b98d9ac6c2436ef124b762456b8f657a27f8f2c8
22P31A4404 <119731243+22P31A4404@users.noreply.github.com>
2023-05-19 18:53:57 +0530

    #include<stdio.h> int main() { 	int n,i,c=0,m=1; 	scanf("%d",&n); 	for(i=1;n>0;) 	{ 	  i=n%10; 	  n=n/10;
    	  c=c+i;
    	  m=m*i;
    	}
    	if(m==c)
    	{
    		printf("Spy Number");
    	}
    	else
    	{
    		printf("Not Spy Number");
    	}
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,s=0,m=1;
-    scanf("%d",&n);
-    for(i=1;n>0;i++)
-    {
-        i=n%10;
-        n=n/10;
-        s=s+i;
-        m=m*i;
-    }
-    if(m==s)
-    {
-        printf("Spy Number");
-    }
-    else
-    {
-        printf("Not Spy Number");
-    }
+	int n,i,c=0,m=1;
+	scanf("%d",&n);
+	for(i=1;n>0;)
+	{
+	  i=n%10;
+	  n=n/10;
+	
+	  c=c+i;
+	  m=m*i;
+	}
+	if(m==c)
+	{
+		printf("Spy Number");
+	}
+	else
+	{
+		printf("Not Spy Number");
+	}
 }
